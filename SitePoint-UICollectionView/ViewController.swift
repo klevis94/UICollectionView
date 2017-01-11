561d50b79e4158a6aa4f5b58b61e3232e6832c95
Klevis Davidhi <klevisdavidhi@yahoo.com>
2017-01-11 11:09:39 +0100

    Final Changes

--- SitePoint-UICollectionView/ViewController.swift
+++ SitePoint-UICollectionView/ViewController.swift
@@ -11,11 +11,24 @@ import UIKit
 class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDataSourcePrefetching {
     
     @IBOutlet weak var collectionView: UICollectionView!
-    let screenSize: CGRect = UIScreen.main.bounds
-   
-    var foodArray = [UIImage](repeating: UIImage(), count: 30)
     
-    let url = URL(string: "https://s-media-cache-ak0.pinimg.com/736x/19/d0/35/19d0354e13202322f8c84e4d459667d5.jpg")
+    let screenSize: CGRect = UIScreen.main.bounds
+    
+    
+    struct food{
+        
+        var image_array : [UIImage]?
+        var source_array : [URL]?
+        
+        init(url: String){
+            self.image_array = [UIImage](repeating: UIImage(), count: 30)
+            self.source_array = [URL](repeating: URL(string: url)!,count: 30)
+        }
+    }
+    
+    
+    var food_instance = food(url: "https://placehold.it")
+    
     
     override func viewDidLoad() {
        
@@ -26,19 +39,24 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         collectionView.prefetchDataSource = self
         
 
-        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+        for index in 0...4{
+    
+            let base_url = food_instance.source_array?[index]
+            
+           
+            let url = url_components(baseUrl: base_url!, index: index)
+            
+            
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else { return }
-            for index in 0...4{
+            
             DispatchQueue.main.async() {
-                self.foodArray[index] = UIImage(data: data)!
+                self.food_instance.image_array?[index] = UIImage(data: data)!
                 self.collectionView.reloadData()
             }
             }
+              task.resume()
         }
-        
-        task.resume()
-        
-    
         
     }
     
@@ -48,45 +66,59 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     }
     
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return foodArray.count
+    func url_components(baseUrl:URL, index: Int) -> URL {
+        
+        var url_components = URLComponents(url: baseUrl, resolvingAgainstBaseURL: true)
+        url_components?.path = "/\(screenSize.width)x\(screenSize.height * 0.3)"
+        url_components?.query = "text=food \(index)"
+        return (url_components?.url)!
     }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return food_instance.image_array!.count
+    }
+    
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let foodCell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCell", for: indexPath) as! collectionViewCell
         
-        foodCell.foodImage.image = foodArray[indexPath.row]
+        foodCell.foodImage.image = food_instance.image_array?[indexPath.row]
         
         return foodCell
     }
 
+    
     func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
         for indexPath in indexPaths{
-
             
-            let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            let base_url = food_instance.source_array?[indexPath.row]
+            let url = url_components(baseUrl: base_url!, index: indexPath.row)
+            
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
                 guard let data = data, error == nil else { return }
-                
-                    self.foodArray[indexPath.row] = UIImage(data: data)!
-                
+               
+                     self.food_instance.image_array?[indexPath.row] = UIImage(data: data)!
             }
             
             task.resume()
-        
-        
+
         }
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
         for indexPath in indexPaths{
-            self.foodArray[indexPath.row] = UIImage()
+            self.food_instance.image_array?[indexPath.row] = UIImage()
         }
     }
-
+    
+    
      func collectionView(_ collectionView: UICollectionView,
                                  layout collectionViewLayout: UICollectionViewLayout,
                                  sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize{
         return CGSize(width:screenSize.width ,height: screenSize.height * 0.3)
     }
+    
 }
 
